06dab6e1a50e972d4816556b25a36d2ae448b476
glebfarmon <glebfarmon@gmail.com>
2025-12-25 20:03:59 +0100

    fix migrate script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-until nc -z ${DB_HOST:-postgres} ${DB_PORT:-5432}; do
+until nc -z ${POSTGRES_HOST:-postgres} ${DB_PORT:-5432}; do
   echo "Database is unavailable - sleeping"
   sleep 1
 done
